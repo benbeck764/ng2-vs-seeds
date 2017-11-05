0a06199ddb9ae8e95e629d12d174e36684de8cc8
CONCURRENCY\Bbeck <bbeck@concurrency.com>
2017-11-05 01:09:13 -0500

    Adding D3 bubble chart example to ng CLI progject

--- PeriGen.POC.NgCli/src/app/components/charts/charts.component.ts
+++ PeriGen.POC.NgCli/src/app/components/charts/charts.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef} from '@angular/core';
+import * as d3 from "d3";
+
+export class TestData {
+  id: string;
+  value: number;
+  ns: string;
+}
 
 @Component({
   selector: 'pg-charts',
@@ -7,9 +14,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartsComponent implements OnInit {
 
-  constructor() { }
+  private parentNativeElement: any;
 
-  ngOnInit() {
+  constructor(element: ElementRef) { 
+    this.parentNativeElement = element.nativeElement;
   }
 
+  // Returns a flattened hierarchy containing all leaf nodes under the root.
+  public classes(root) {
+    var classes = [];
+
+    function recurse(name, node) {
+      if (node.children) node.children.forEach(function (child) { recurse(node.name, child); });
+      else classes.push({ packageName: name, className: node.name, value: node.size });
+    }
+
+    recurse(null, root);
+    return { children: classes };
+  }
+
+  ngOnInit() {
+    let width: number;
+    let height: number;
+
+    if (this.parentNativeElement !== null) {
+
+      var diameter = 960;
+      var format = d3.format(",d");
+      var color = d3.scaleOrdinal(d3.schemeCategory20c);
+
+      var bubble = d3.pack()
+        .size([diameter, diameter])
+        .padding(1.5);
+
+      var svg = d3.select("#bubble-chart").append("svg")
+        .attr("width", diameter)
+        .attr("height", diameter)
+        .attr("class", "bubble");
+
+      d3.json("assets/flare.json",
+        (error, data) => {
+          if (error) throw error;
+
+          var root = d3.hierarchy(this.classes(data))
+            .sum(d => d.value)
+            .sort((a, b) => b.value - a.value);
+
+          bubble(root);
+          var node = svg.selectAll(".node")
+            .data(root.children)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", d => "translate(" + d.x + "," + d.y + ")");
+
+          node.append("title")
+            .text(d => d.data.className + ": " + format(d.value));
+
+          node.append("circle")
+            .attr("r", d => d.r)
+            .style("fill",
+              d => color(d.data.packageName));
+
+          node.append("text")
+            .attr("dy", ".3em")
+            .style("text-anchor", "middle")
+            .text(d => d.data.className.substring(0, d.r / 3));
+        });
+
+        d3.select(self.frameElement).style("height", diameter + "px");
+    }
+  }
 }
